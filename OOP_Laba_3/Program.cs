6eb9564cb9ac3f995d5b3323bb3cec7ba4a913ef
Vitaly1123 <nezhura.vitalii1123@vu.cdu.edu.ua>
2024-12-09 22:47:34 +0200

    Додав (main) та метод (ProcessFile).

--- OOP_Laba_3/Program.cs
+++ OOP_Laba_3/Program.cs
@@ -1,10 +1,62 @@
 ﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+
 
 class Program 
 {
     static void Main() 
     {
+        // Списки для збереження результатів
+        List<int> validProducts = new List<int>();
+        List<string> noFiles = new List<string>();
+        List<string> badDataFiles = new List<string>();
+        List<string> overflowFiles = new List<string>();
+
+        for (int i = 10; i < 30; i++)
+        {
+            string fileName = $"{i}.txt";
+            ProcessFile(fileName, validProducts, noFiles, badDataFiles, overflowFiles);
+        }
+
 
     }
-    
+    static void ProcessFile(string fileName, List<int> validProducts, List<string> noFiles, List<string> badDataFiles, List<string> overflowFiles)
+    {
+        try
+        {
+            string[] lines = File.ReadAllLines(fileName);
+            int firstNumber, secondNumber;
+
+            if (!int.TryParse(lines[0], out firstNumber) || !int.TryParse(lines[1], out secondNumber))
+            {
+                badDataFiles.Add(fileName);
+                return;
+            }
+
+            try
+            {
+                checked
+                {
+                    int product = firstNumber * secondNumber;
+                    validProducts.Add(product);
+                }
+            }
+            catch (OverflowException)
+            {
+                overflowFiles.Add(fileName);
+            }
+        }
+        catch (FileNotFoundException)
+        {
+            noFiles.Add(fileName);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Помилка при обробці файлу {fileName}: {ex.Message}");
+        }
+    }
+
+
 }
